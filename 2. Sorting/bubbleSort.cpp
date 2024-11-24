dfcd439d62d14923f2a34555be64dd067fe7e85e
Vishav Gupta <singlasavita91@gmail.com>
2024-11-25 01:09:21 +0530

    Update bubbleSort.cpp

--- 2. Sorting/bubbleSort.cpp
+++ 2. Sorting/bubbleSort.cpp
@@ -13,9 +13,7 @@ void bubbleSort(int arr[], int len) {
         int cnt = 0;
         for(int j = 0; j < len - i - 1; j++) {
             if(arr[j] > arr[j+1]) {
-                int temp = arr[j];
-                arr[j] = arr[j+1];
-                arr[j+1] = temp;
+                swap(arr[j], arr[j+1]);
                 cnt++;
             }
         }
